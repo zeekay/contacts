e5d794111f8d887e207cc32905bf2f6538f06ebc
zeekay <zeekayy@gmail.com>
2012-07-10 18:11:00 -0500

    Add new user form.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
 var User = Backbone.Model.extend({
-  urlRoot: '/api/users'
+  urlRoot: '/api/users',
+  defaults: {
+    name: 'Name',
+    age: 0
+  }
 });
 
 var Users = Backbone.Collection.extend({
@@ -7,8 +11,56 @@ var Users = Backbone.Collection.extend({
   url: '/api/users'
 });
 
+var UserForm = Backbone.View.extend({
+  el: $('#new-user'),
+
+  initialize: function() { this.render(); },
+  render: function(){
+    var that = this;
+
+    // Create a new user which we will save to the server.
+    this.user = new User();
+
+    // Loop default attributes in user and create form
+    for (attrib in this.user.attributes) {
+      // Label for attribute
+      this.$el.append('<label>' + attrib + '</label>');
+      // Create new input to set user attribute
+      var $input = $('<input type="text" placeholder="' + this.user.get(attrib) + '"></input>');
+
+      // Bind to change event, set attribute on user when input is changed.
+      (function($input, attrib, user){
+        $input.on('change', function(e) {
+          user.set(attrib, $input.val());
+        })
+      })($input, attrib, this.user);
+
+      this.$el.append($input);
+    }
+
+    this.$el.append('<input type="submit">')
+
+    // Bind to submit, create new user and re-render empty form.
+    this.$el.on('submit', function(e) {
+      that.user.save();
+      that.render();
+    })
+
+    return this;
+  }
+})
+
 var UserView = Backbone.View.extend({
-  tagName: 'li',
+  tagName: 'p',
+
+  events: {
+    'click li.destroy': 'destroy'
+  },
+
+  destroy: function(e){
+    console.log(e);
+    this.model.destroy();
+  },
 
   template: _.template($('#user-template').html()),
 
@@ -30,6 +82,7 @@ var UsersView = Backbone.View.extend({
 
     // Create instance of Users collection.
     this.users = new Users();
+    this.users.bind('all', this.render, this);
 
     // So we can play with users in browser.
     window.users = this.users;
@@ -61,3 +114,4 @@ var UsersView = Backbone.View.extend({
 });
 
 usersView = new UsersView();
+userForm = new UserForm();
