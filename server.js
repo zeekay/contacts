196d46cb6a8b6ffd9c90fc9246c578893539cf28
zeekay <zeekayy@gmail.com>
2012-07-11 02:01:49 -0500

    Use in-memory db.

--- server.js
+++ server.js
@@ -1,74 +1,62 @@
+var dirty = require('dirty');
 var express = require('express');
 var uuid = require('node-uuid');
 
+// Create new database
+var db = dirty(__dirname + '/users.db');
+
+// Create new express app
 var app = express.createServer();
 
-// Server files from /public
+// Log requests
+app.use(express.logger());
+// Serve static files in ./public
 app.use(express.static(__dirname + '/public'));
+// Parse json sent to use from Backbone.js
 app.use(express.bodyParser());
 
-// Users RESTful API
+// Backbone Users model RESTful API
+// --------------------------------
+// Backbone expects the typical CRUD methods to map to these HTTP methods:
 //
-// CRUD:
-//
-// Create   -> POST
-// Read     -> GET
-// Update   -> PUT
-// Delete   -> DELETE
-
-var users = {
-  '61e9c450-cc79-457c-b717-16a70b4a9ef8': {
-    name: 'Nate',
-    age: 31
-  },
-  '29c5c99f-6f39-4990-af7b-a8923cf6a96d': {
-    name: 'Tom',
-    age: 44
-  }
-};
+// Create -> POST
+// Read   -> GET
+// Update -> PUT
+// Delete -> DELETE
 
 // Get a list of users
 app.get('/api/users', function(req, res) {
-  console.log('GET /api/users');
   var collection = [];
-  for (var id in users) {
-    var user = users[id];
-    user.id = id;
+  db.forEach(function(id, user) {
     collection.push(user);
-  }
-  res.json(collection, 200);
+  })
+  res.json(collection);
 });
 
 // Get a single model
 app.get('/api/users/:id', function(req, res) {
-  console.log('GET /api/users/' + req.params.id);
-  var user = users[id];
-  res.json(user, 200);
+  var user = db.get(id);
+  res.json(user);
 });
 
 // Create a new User
 app.post('/api/users', function(req, res) {
-  console.log('POST /api/users', req.body);
   var user = req.body;
   var id = uuid.v4();
 
-  users[id] = user;
+  db.set(id, user);
   res.send('ok')
 });
 
 // Update a User
 app.put('/api/users/:id', function(req, res) {
-  console.log('PUT /api/users/' + req.params.id, req.body);
-  // Update user with new body
-  users[req.params.id] = req.body;
+  db.set(req.params.id, req.body);
   res.send('ok')
 });
 
 // Delete a User
 app.del('/api/users/:id', function(req, res) {
-  console.log('DELETE /api/users/' + req.params.id);
-  // delete user by name
-  delete users[req.params.id]
+  db.rm(req.params.id);
   res.send('ok')
 });
 
